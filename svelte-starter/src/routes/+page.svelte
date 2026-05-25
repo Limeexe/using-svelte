16344919fbc6a532752f5192e37711b13adc835e
Limeexe <johnlouieabenir@gmail.com>
2026-05-25 22:35:51 +0800

    Using the Runes

--- svelte-starter/src/routes/+page.svelte
+++ svelte-starter/src/routes/+page.svelte
@@ -3,12 +3,20 @@
 
     let doubleCount = $derived(count *2);
 
+    $effect(()=>{
+        console.log(`Count: ${count}, Double Count: ${doubleCount}`);
+    });
+
+    function increment(){
+        count++;
+    }
+
     function reset(){
         count = 0;
     }
 </script>
 
-<button onclick={()=>count++}>Increment</button>
+<button onclick={increment}>Increment</button>
 <button onclick={reset}> Reset</button>
 
 <h1>Count: {count} <br> Double Count: {doubleCount}</h1>
